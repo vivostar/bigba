5237c0d289d63dda182b3e22a211ceb4238d8394
vivostar <lishuaipeng651@gmail.com>
2022-04-01 01:40:13 +0800

    make config.js more readable

--- config.js
+++ config.js
@@ -15,7 +15,33 @@ module.exports.config = {
         'test/javascripts/test-vendor.js': /^test(\/|\\)(?=vendor)/
       },
       order: {
-        before: ['vendor/scripts/console-helper.js', 'vendor/scripts/jquery-1.7.2.min.js', 'vendor/scripts/handlebars-1.0.0.beta.6.js', 'vendor/scripts/ember-latest.js', 'vendor/scripts/ember-data-latest.js', 'vendor/scripts/ember-i18n-1.2.0.js', 'vendor/scripts/bootstrap.js', 'vendor/scripts/bootstrap-combobox.js', 'vendor/scripts/d3.v2.js', 'vendor/scripts/cubism.v1.js', 'vendor/scripts/jquery.ui.core.js', 'vendor/scripts/jquery.ui.widget.js', 'vendor/scripts/jquery.ui.mouse.js', 'vendor/scripts/jquery.ui.datepicker.js', 'vendor/scripts/jquery-ui-timepicker-addon.js', 'vendor/scripts/jquery.ui.slider.js', 'vendor/scripts/jquery.ui.sortable.js', 'vendor/scripts/jquery.ui.custom-effects.js', 'vendor/scripts/jquery.timeago.js', 'vendor/scripts/jquery.ajax-retry.js', 'vendor/scripts/workflow_visualization.js', 'vendor/scripts/rickshaw.js', 'vendor/scripts/spin.js', 'vendor/scripts/jquery.flexibleArea.js', 'vendor/scripts/FileSaver.js', 'vendor/scripts/Blob.js']
+        before: [
+          'vendor/scripts/console-helper.js', 
+          'vendor/scripts/jquery-1.7.2.min.js',
+          'vendor/scripts/handlebars-1.0.0.beta.6.js', 
+          'vendor/scripts/ember-latest.js', 
+          'vendor/scripts/ember-data-latest.js', 
+          'vendor/scripts/ember-i18n-1.2.0.js', 
+          'vendor/scripts/bootstrap.js', 
+          'vendor/scripts/bootstrap-combobox.js', 
+          'vendor/scripts/d3.v2.js', 
+          'vendor/scripts/cubism.v1.js', 
+          'vendor/scripts/jquery.ui.core.js', 
+          'vendor/scripts/jquery.ui.widget.js', 
+          'vendor/scripts/jquery.ui.mouse.js', 
+          'vendor/scripts/jquery.ui.datepicker.js', 
+          'vendor/scripts/jquery-ui-timepicker-addon.js', 
+          'vendor/scripts/jquery.ui.slider.js', 
+          'vendor/scripts/jquery.ui.sortable.js', 
+          'vendor/scripts/jquery.ui.custom-effects.js', 
+          'vendor/scripts/jquery.timeago.js', 
+          'vendor/scripts/jquery.ajax-retry.js', 
+          'vendor/scripts/workflow_visualization.js', 
+          'vendor/scripts/rickshaw.js', 
+          'vendor/scripts/spin.js', 
+          'vendor/scripts/jquery.flexibleArea.js', 
+          'vendor/scripts/FileSaver.js', 
+          'vendor/scripts/Blob.js']
       }
     },
     stylesheets: {
